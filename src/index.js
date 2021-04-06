bcbc50459d32dbbc3144df5d104a3e35f227f115
jason-gkq <guokeqin163@163.com>
2021-04-06 10:11:40 +0800

    tianjia

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "babel-polyfill";
+
+import React from "react";
+import { render } from "react-dom";
+// import Root from "./containers/Root";
+import App from "./App";
+
+render(<App />, document.getElementById("root"));
